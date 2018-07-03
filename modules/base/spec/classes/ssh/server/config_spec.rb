3dde5b42b92391eaa17bdbcfd79b130d27cf0b29
ShIfT <shift@unrulygroup.com>
2018-07-03 16:10:55 +0000

    feat(base): Expands tests to include sshd_config contents.
    Tests for most critical configuration setup.
    
    Co-authored-by: Ina Tsetsova <inatsetsova@gmail.com>
    Co-authored-by: Sarah Young <sarahseewhy@gmail.com>

--- modules/base/spec/classes/ssh/server/config_spec.rb
+++ modules/base/spec/classes/ssh/server/config_spec.rb
@@ -6,4 +6,12 @@ describe 'base::ssh::server::config' do
     :owner  => 'root',
     :group  => 'root',
   })}
+
+  it { is_expected.to contain('Protocol 2')}
+  it { is_expected.to contain('PasswordAuthentication no')}
+  it { is_expected.to contain('PermitRootLogin no')}
+end
+
+def contain(configuration)
+  contain_file('/etc/ssh/sshd_config').with_content(/^#{configuration}$/)
 end

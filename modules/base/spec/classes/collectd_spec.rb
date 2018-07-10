f25e862d32f9e82a234d60e0bbf983bb56bc0d31
ShIfT <shift@unrulygroup.com>
2018-07-10 15:56:10 +0000

    feat(base-collectd): Set up graphite and unixsock collectd configuration.
    Co-authored-by: Sarah Young <sarahseewhy@gmail.com>
    Co-authored-by: Stephen Geller <stephen.geller@hotmail.com>

--- modules/base/spec/classes/collectd_spec.rb
+++ modules/base/spec/classes/collectd_spec.rb
@@ -36,7 +36,35 @@ describe 'base::collectd' do
 
   it {
     base_plugins = %w(contextswitch cpu cpufreq df disk entropy interface irq load memory processes protocols swap table uptime users)
-    is_expected_to_have_plugins('/etc/collectd.d/base.conf', base_plugins) }
+    is_expected_to_have_plugins('/etc/collectd.d/base.conf', base_plugins)
+  }
+
+  it { is_expected.to  contain_file('/etc/collectd.d/graphite.conf')
+        .with(
+            :owner  => 'root',
+            :group  => 'root',
+            :mode   => '0644')
+  }
+
+  it {
+    is_expected_to_have_plugins('/etc/collectd.d/graphite.conf', %w(write_graphite))
+    is_expected.to  contain_file('/etc/collectd.d/graphite.conf')
+        .with_content(/\s+Host "some-host"$/)
+        .with_content(/\s+Port "some-port"$/)
+    is_expected_to_have_config('/etc/collectd.d/graphite.conf', 'write_graphite')
+  }
+
+  it { is_expected.to  contain_file('/etc/collectd.d/unixsock.conf')
+        .with(
+            :owner  => 'root',
+            :group  => 'root',
+            :mode   => '0644')
+  }
+
+  it {
+    is_expected_to_have_plugins('/etc/collectd.d/unixsock.conf', %w(unixsock))
+    is_expected_to_have_config('/etc/collectd.d/unixsock.conf', 'unixsock')
+  }
 end
 
 def is_expected_to_have_plugins(file, plugins)
@@ -45,4 +73,8 @@ def is_expected_to_have_plugins(file, plugins)
   end
 end
 
-
+def is_expected_to_have_config(file, config)
+    is_expected.to contain_file(file)
+           .with_content(/^<Plugin #{config}>$/)
+           .with_content(/^<\/Plugin>$/)
+end

0a4242702973b4fb9080c551457949b5edc4ed1c
ShIfT <shift@unrulygroup.com>
2018-07-10 14:58:26 +0000

    feat(base-collectd): Set up base plugin configuration.
    * Does not include:
     - conntrack, as it requires the configuration of iptables
     - vmem, as it requires config beyond base configuration.
    
    Co-authored-by: Sarah Young <sarahseewhy@gmail.com>
    Co-authored-by: Stephen Geller <stephen.geller@hotmail.com>

--- modules/base/spec/classes/collectd_spec.rb
+++ modules/base/spec/classes/collectd_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe 'base::collectd' do
+
   it { is_expected.to compile }
 
   it { is_expected.to contain_package('collectd')
@@ -20,9 +21,28 @@ describe 'base::collectd' do
             :mode   => '0644')
   }
 
-  it { is_expected.to  contain_file('/etc/collectd.conf')
-        .with_content(/^LoadPlugin "logfile"$/)
+  it {
+    is_expected_to_have_plugins('/etc/collectd.conf',['\"logfile\"'])
+    is_expected.to  contain_file('/etc/collectd.conf')
         .with_content(/^Include "\/etc\/collectd.d\/\*.conf"$/)
   }
 
+  it { is_expected.to  contain_file('/etc/collectd.d/base.conf')
+        .with(
+            :owner  => 'root',
+            :group  => 'root',
+            :mode   => '0644')
+  }
+
+  it {
+    base_plugins = %w(contextswitch cpu cpufreq df disk entropy interface irq load memory processes protocols swap table uptime users)
+    is_expected_to_have_plugins('/etc/collectd.d/base.conf', base_plugins) }
 end
+
+def is_expected_to_have_plugins(file, plugins)
+  plugins.each do |plugin|
+    is_expected.to contain_file(file).with_content(/^LoadPlugin #{plugin}$/)
+  end
+end
+
+

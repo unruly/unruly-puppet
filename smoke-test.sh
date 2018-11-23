b8d3eaf7b2b4de50919df65919f4feb316cb4a70
ShIfT <shift@unrulygroup.com>
2018-11-23 18:18:06 +0000

    feat(smoke-test): UX Improvements to smoke test script
    Co-authored-by: Ina Tsetsova <inatsetsova@gmail.com>
    Co-authored-by: Narayan O'Hanlon <narayanohanlon@gmail.com>

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 CLASS_TO_IMPORT=$1
 IMAGE_NAME=${2:-image}
@@ -13,13 +12,25 @@ fi
 
 #Build docker image
 echo -e "\n========== BUILDING =========\n"
-docker build --no-cache -t ${IMAGE_NAME} .
+docker build -t ${IMAGE_NAME} .
 
-#Start docker container
+# Start docker container
 echo -e "\n========== RUNNING =========\n"
-#First run systemd (Dockerfile CMD takes care of this)
+# First run systemd (Dockerfile CMD takes care of this)
 docker run --rm -d --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name ${CONTAINER_NAME} ${IMAGE_NAME}
-#Then run puppet
-docker exec -t ${CONTAINER_NAME} /opt/puppetlabs/bin/puppet apply --modulepath /puppet/modules/ -e "include ${CLASS_TO_IMPORT}"
+# Then run puppet
+docker exec -t ${CONTAINER_NAME} /bin/sh -c "/opt/puppetlabs/bin/puppet apply --detailed-exitcodes --modulepath /puppet/modules/ -e 'include ${CLASS_TO_IMPORT}'"
+DOCKER_EXIT_CODE=$?
 
-destroy_docker_container.sh ${CONTAINER_NAME}
+echo "${CONTAINER_NAME} exited with code ${DOCKER_EXIT_CODE}."
+
+if [[ ${DOCKER_EXIT_CODE} -eq 2 ]]; then
+  GREEN='\033[0;32m'
+  NC='\033[0m' # No Color
+  printf "${GREEN}Manifests applied without errors.${NC}\n"
+fi
+
+# Stop the container
+docker stop ${CONTAINER_NAME} > /dev/null
+# Remove all untagged images (i.e. everything except the 'latest' image)
+docker rmi $(docker images | grep "^<none>" | awk '{print $3}') 2>&1 > /dev/null

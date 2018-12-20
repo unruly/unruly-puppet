47457ca357dad0df9895ff28ee57939c46380ac9
ShIfT <shift@unrulygroup.com>
2018-12-20 16:11:59 +0000

    feat(nrpe_custom_checks): Define plugin resource
    * This manages an individual nrpe plugin script.
    
    Co-authored-by: Ina Tsetsova <inatsetsova@gmail.com>
    Co-authored-by: Narayan O'Hanlon <narayanohanlon@gmail.com>

---
+++ modules/nrpe_custom_check/manifests/plugin.pp
@@ -0,0 +1,33 @@
+# == Define: nrpe_custom_check::plugin
+#
+# Manages an individual nrpe plugin script.
+#
+# === Parameters:
+#
+# [*plugin_module_path*]
+#   Module in which to find the plugin script.
+#
+# [*ensure*]
+#   Whether the plugin file should exist or not. Can be 'present' or 'absent'.
+#
+# [*nrpe_class*]
+#   The name of the puppet class which sets up and installs nrpe.
+#
+# [*nrpe_plugins_dir*]
+#   The absolute path to the directory containing the nrpe plugins.
+#
+define nrpe_custom_check::plugin(
+  $plugin_module_path,
+  $ensure = 'present',
+  $nrpe_class = hiera('nrpe_custom_check::plugin::nrpe_class'),
+  $nrpe_plugins_dir = hiera('nrpe_custom_check::plugins_directory'),
+) {
+  file { "${nrpe_plugins_dir}/${name}":
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => [Class[$nrpe_class]],
+    source  => "puppet:///modules/${plugin_module_path}/${name}",
+  }
+}
---
+++ modules/nrpe_custom_check/spec/defines/plugin_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'nrpe_custom_check::plugin' do
+  let(:title) { 'plugin.sh' }
+
+  let(:params) do
+    {
+        :plugin_module_path => "module_path"
+    }
+  end
+
+  let(:pre_condition) { 'include base::nrpe' }
+
+  it { is_expected.to compile }
+  it { is_expected.to contain_file('/some_path/plugin.sh')
+    .with(
+      :ensure => 'present',
+      :group => 'root',
+      :owner => 'root',
+      :mode  => '0755'
+    )
+    .that_requires('Class[base::nrpe]')
+    .with_source('puppet:///modules/module_path/plugin.sh')
+  }
+end
